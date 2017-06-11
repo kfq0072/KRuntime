28e7227d5565acd990c1fa56c7d97f13ddf76507
kfq <kfq0072@163.com>
2017-06-11 16:33:52 +0800

    modify

--- MyRunTime/MyRunTime/Add/DyAddNewClassViewController.m
+++ MyRunTime/MyRunTime/Add/DyAddNewClassViewController.m
@@ -63,8 +63,9 @@
     class_addProperty(kclass, "name", attrs, 3);
     
     //添加方法
-    class_addMethod(kclass, @selector(getFatherName), (IMP)getFatherName, "v@:");
-    [RuntimeKit addMethod:kclass method:@selector(setFatherName:) methodImp:@selector(setFatherName:)];
+    class_addMethod(kclass, @selector(setNoParamFunction), (IMP)setNoParamFunction, "v@:");
+//    class_addMethod(kclass, @selector(setOneParamFunction:), (IMP)setOneParamFunction, "v@:@");
+    [RuntimeKit addMethod:kclass method:@selector(setOneParamFunction:) methodImp:@selector(setOneParamFunction:)];
     //TODO:添加协议
 //    class_addProtocol(kclass, fatherp)
     objc_registerClassPair(kclass);
@@ -73,10 +74,10 @@
     
 }
 
-static void setFatherName(id self,SEL cmd,id value) {
-    NSLog(@"call setFatherName");
+static void setOneParamFunction(id self,SEL cmd,id value) {
+    NSLog(@"call setOneParamFunction");
 }
-static void getFatherName(id self,SEL cmd) {
-    NSLog(@"call getFatherName");
+static void setNoParamFunction(id self,SEL cmd) {
+    NSLog(@"call setNoParamFunction");
 }
 @end
